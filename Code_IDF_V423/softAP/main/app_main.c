da9f950b4814f89adde49c28e50867b10cf9a9a7
NguyenDucThuan_TH <thuan.nd2709@gmail.com>
2024-05-12 00:01:22 +0700

    add smartconfig - store the wifi infor in flash

--- Code_IDF_V423/softAP/main/app_main.c
+++ Code_IDF_V423/softAP/main/app_main.c
@@ -20,6 +20,7 @@
 #include "lwip/sys.h"
 
 #include "app_http_server.h"
+#include "esp_smartconfig.h"
 
 /* The examples use WiFi configuration that you can set via project configuration menu.
 
@@ -33,10 +34,22 @@
 
 static const char *TAG = "wifi softAP";
 
+/* Select solution for connecting between esp and mobile */
+typedef enum{
+    PROV_NONE,
+    PROV_SMART_CONFIG,
+    PROV_ACCESS_POINT
+} provision_type_t;
+
+static void smartconfig_example_task(void * parm);
+
 EventGroupHandle_t xCreatedEventGroup;
 #define WIFI_RECV_INFO	BIT0
 #define WIFI_FAIL_BIT	BIT1
 #define WIFI_CONNECTED_BIT	BIT2
+#define ESPTOUCH_DONE_BIT BIT3
+
+provision_type_t provision_type = PROV_SMART_CONFIG;
 
 static void wifi_event_handler(void* arg, esp_event_base_t event_base,
                                     int32_t event_id, void* event_data)
@@ -76,6 +89,56 @@ static void wifi_event_handler(void* arg, esp_event_base_t event_base,
     }
 }
 
+static void event_handler(void* arg, esp_event_base_t event_base, 
+                                int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        if (provision_type == PROV_NONE)
+        {
+            esp_wifi_connect();
+        }
+        else if (provision_type == PROV_SMART_CONFIG)
+        {
+            xTaskCreate(smartconfig_example_task, "smartconfig_example_task", 4096, NULL, 3, NULL);
+        }
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        esp_wifi_connect();
+        xEventGroupClearBits(xCreatedEventGroup, WIFI_CONNECTED_BIT);
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        xEventGroupSetBits(xCreatedEventGroup, WIFI_CONNECTED_BIT);
+    } else if (event_base == SC_EVENT && event_id == SC_EVENT_SCAN_DONE) {
+        ESP_LOGI(TAG, "Scan done");
+    } else if (event_base == SC_EVENT && event_id == SC_EVENT_FOUND_CHANNEL) {
+        ESP_LOGI(TAG, "Found channel");
+    } else if (event_base == SC_EVENT && event_id == SC_EVENT_GOT_SSID_PSWD) {
+        ESP_LOGI(TAG, "Got SSID and password");
+
+        smartconfig_event_got_ssid_pswd_t *evt = (smartconfig_event_got_ssid_pswd_t *)event_data;
+        wifi_config_t wifi_config;
+        uint8_t ssid[33] = { 0 };
+        uint8_t password[65] = { 0 };
+
+        bzero(&wifi_config, sizeof(wifi_config_t));
+        memcpy(wifi_config.sta.ssid, evt->ssid, sizeof(wifi_config.sta.ssid));
+        memcpy(wifi_config.sta.password, evt->password, sizeof(wifi_config.sta.password));
+        wifi_config.sta.bssid_set = evt->bssid_set;
+        if (wifi_config.sta.bssid_set == true) {
+            memcpy(wifi_config.sta.bssid, evt->bssid, sizeof(wifi_config.sta.bssid));
+        }
+
+        memcpy(ssid, evt->ssid, sizeof(evt->ssid));
+        memcpy(password, evt->password, sizeof(evt->password));
+        ESP_LOGI(TAG, "SSID:%s", ssid);
+        ESP_LOGI(TAG, "PASSWORD:%s", password);
+
+        ESP_ERROR_CHECK( esp_wifi_disconnect() );
+        ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+        esp_wifi_connect();
+    } else if (event_base == SC_EVENT && event_id == SC_EVENT_SEND_ACK_DONE) {
+        xEventGroupSetBits(xCreatedEventGroup, ESPTOUCH_DONE_BIT);
+    }
+}
+
 char ssid[64];
 char pwd[32];
 
@@ -112,7 +175,7 @@ void wifi_init_sta(void)
     /* Stop access point mode before entering station mode */
     esp_wifi_stop();
 
-    esp_netif_create_default_wifi_sta();
+    // esp_netif_create_default_wifi_sta();
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
@@ -142,8 +205,6 @@ void wifi_init_sta(void)
 
 void wifi_init_softap(void)
 {
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
     esp_netif_create_default_wifi_ap();
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
@@ -194,6 +255,35 @@ void wifi_init_softap(void)
     wifi_init_sta();
 }
 
+static void initialise_wifi(void)
+{
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
+    assert(sta_netif);
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+}
+
+static void smartconfig_example_task(void * parm)
+{
+    EventBits_t uxBits;
+    ESP_ERROR_CHECK( esp_smartconfig_set_type(SC_TYPE_ESPTOUCH) );
+    smartconfig_start_config_t cfg = SMARTCONFIG_START_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK( esp_smartconfig_start(&cfg) );
+    while (1) {
+        uxBits = xEventGroupWaitBits(xCreatedEventGroup,  ESPTOUCH_DONE_BIT, true, false, portMAX_DELAY);
+        if(uxBits & ESPTOUCH_DONE_BIT) {
+            ESP_LOGI(TAG, "smartconfig over");
+            esp_smartconfig_stop();
+            vTaskDelete(NULL);
+        }
+    }
+}
+
 void app_main(void)
 {
     //Initialize NVS
@@ -206,6 +296,36 @@ void app_main(void)
     /* Attempt to create the event group. */
     xCreatedEventGroup = xEventGroupCreate();
 
-    ESP_LOGI(TAG, "ESP_WIFI_MODE_AP");
-    wifi_init_softap();
+    initialise_wifi();
+    wifi_config_t conf;
+    esp_wifi_get_config(ESP_IF_WIFI_STA, &conf);
+    if (conf.sta.ssid[0] != 0)
+    {
+        provision_type = PROV_NONE;
+        ESP_ERROR_CHECK( esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL) );
+        ESP_ERROR_CHECK( esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL) );
+        ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &conf) );
+        ESP_ERROR_CHECK(esp_wifi_start() );
+
+    }
+    else
+    {
+        ESP_LOGI(TAG, "ESP_WIFI_MODE_AP");
+        /* At this point, esp connected to wifi successfully */
+        if (provision_type == PROV_ACCESS_POINT)
+        {
+            wifi_init_softap();
+        }
+        else if (provision_type == PROV_SMART_CONFIG)
+        {
+            ESP_ERROR_CHECK( esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL) );
+            ESP_ERROR_CHECK( esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL) );
+            ESP_ERROR_CHECK( esp_event_handler_register(SC_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL) );
+            ESP_ERROR_CHECK( esp_wifi_start() );
+        }
+    }
+
+    /* Task main will be blocked until esp connected to wifi successfully */
+    xEventGroupWaitBits(xCreatedEventGroup,  WIFI_CONNECTED_BIT, true, false, portMAX_DELAY);
+    ESP_LOGI(TAG, "Ready to connect to cloud\n");
 }
